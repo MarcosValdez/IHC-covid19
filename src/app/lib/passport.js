f21243502624a60a83abbbf28f88df17495e9ded
EDGUS1 <eduardo.navarro2@unmsm.edu.pe>
2021-01-03 22:46:26 -0500

    fix heroku-validate

--- src/app/lib/passport.js
+++ src/app/lib/passport.js
@@ -91,11 +91,19 @@ passport.use('local.validate', new LocalStrategy({
             aux = '0'.concat(aux);
         }
         segundaFecha.push(aux);
-
+        console.log(primeraFecha);
+        console.log(segundaFecha);
         validPassword = false;
+
+        //localhost
         if (primeraFecha[0] == segundaFecha[2] && primeraFecha[1] == segundaFecha[1] && primeraFecha[2] == segundaFecha[0]) {
             validPassword = true;
         }
+
+        //en despliegue
+        /* if (primeraFecha[0] == segundaFecha[2] && primeraFecha[1] == segundaFecha[0] && primeraFecha[2] == segundaFecha[1]) {
+            validPassword = true;
+        } */
         if (validPassword) {
             done(null, user, req.flash('success', [user.dni, user.id_hospital]));
         } else {
