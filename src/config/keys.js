b36ecd1a8b027034e0e5490f69f0233b1fc2e93f
EDGUS1 <eduardo.navarro2@unmsm.edu.pe>
2021-01-07 01:18:32 -0500

    vspansvop

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,8 +1,12 @@
 module.exports = {
     database: {
-        host: 'bbqawibtcfyfmjgv0usz-mysql.services.clever-cloud.com',
+        /* host: 'bbqawibtcfyfmjgv0usz-mysql.services.clever-cloud.com',
         user: 'uhywjyex8jbxjeox',
         password: 'QnGKOhD8Heed8htTO8OJ',
-        database: 'bbqawibtcfyfmjgv0usz'
+        database: 'bbqawibtcfyfmjgv0usz' */
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'ihc'
     }
 };
