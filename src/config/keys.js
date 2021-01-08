2a66e4d1c034a9161d01d3120d9b065a290a8416
MarcosV333 <marcos.valdez333@hotmail.com>
2021-01-07 21:16:26 -0500

    dasdfa

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,12 +1,8 @@
 module.exports = {
     database: {
-        /* host: 'bbqawibtcfyfmjgv0usz-mysql.services.clever-cloud.com',
+        host: 'bbqawibtcfyfmjgv0usz-mysql.services.clever-cloud.com',
         user: 'uhywjyex8jbxjeox',
         password: 'QnGKOhD8Heed8htTO8OJ',
-        database: 'bbqawibtcfyfmjgv0usz' */
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'ihc'
+        database: 'bbqawibtcfyfmjgv0usz' 
     }
 };
