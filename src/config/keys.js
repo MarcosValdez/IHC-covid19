95a87b57f8c3da074577b9e87014e4e816ac2fa0
EDGUS1 <eduardo.navarro2@unmsm.edu.pe>
2021-01-07 19:41:00 -0500

    voiwnviw

--- src/config/keys.js
+++ src/config/keys.js
@@ -3,14 +3,10 @@ module.exports = {
         host: 'bbqawibtcfyfmjgv0usz-mysql.services.clever-cloud.com',
         user: 'uhywjyex8jbxjeox',
         password: 'QnGKOhD8Heed8htTO8OJ',
-<<<<<<< HEAD
         database: 'bbqawibtcfyfmjgv0usz'
             /* host: 'localhost',
             user: 'root',
             password: '',
             database: 'ihc' */
-=======
-        database: 'bbqawibtcfyfmjgv0usz' 
->>>>>>> Marcos333
     }
 };
